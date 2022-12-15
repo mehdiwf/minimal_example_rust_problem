f5b5cc7c02621cba300787e172abb6aef79afb03
Mehdi Fawaz <mehdi.fawaz@univ-lyon1.fr>
2022-12-15 16:13:13 +0100

    simplified code and added comments

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@
 use ndarray::prelude::*;
 use rayon::prelude::*;
 
+// -----------------------------------------
+// STRUCTS AND METHOD DEFINITION
+// -----------------------------------------
+
 #[derive(Debug, PartialEq)]
 struct vec2D {pub x: f64, pub y: f64}
 
@@ -58,11 +62,14 @@ enum DerivDirection {
     X_axis,
     Y_axis}
 
+// -----------------------------------------
+// FUNCTIONS DEFINITION
+// -----------------------------------------
+
 fn partial_deriv(a: &ScalarField2D,
                  x: i32, y: i32,
                  direction: DerivDirection,
-                 x_max: i32, y_max: i32,
-                 dx: f64, dy:f64) -> f64
+                 x_max: i32, y_max: i32) -> f64
 {
     // i+1 with Periodic Boundaries
     let ip = ((x+1) % x_max) as usize;
@@ -78,38 +85,39 @@ fn partial_deriv(a: &ScalarField2D,
         {
             DerivDirection::X_axis => {
                 let derivative =
-                    (a.get_pos(ip, j) - a.get_pos(im, j))/(2.*dx);
+                    (a.get_pos(ip, j) - a.get_pos(im, j))/(2.);
                 return derivative;
             },
             DerivDirection::Y_axis => {
                 let derivative =
-                    (a.get_pos(i, jp) - a.get_pos(i, jm))/(2.*dy);
+                    (a.get_pos(i, jp) - a.get_pos(i, jm))/(2.);
                 return derivative;
             }}}
 
+// computes the gradient of a scalar field at a given position
 fn grad_scalar(scalar_field: &ScalarField2D,
                x: i32, y: i32,
-               x_max: i32, y_max: i32,
-               dx: f64, dy:f64) -> vec2D
+               x_max: i32, y_max: i32) -> vec2D
 {
     let grad = vec2D {
         x: partial_deriv(&scalar_field, x, y,
                          DerivDirection::X_axis,
-                         x_max, y_max,
-                         dx, dy),
+                         x_max, y_max),
         y: partial_deriv(&scalar_field, x, y,
                          DerivDirection::Y_axis,
-                         x_max, y_max,
-                         dx, dy),};
+                         x_max, y_max)};
         
     return grad;
 }
+
+// -----------------------------------------
+// MAIN
+// -----------------------------------------
 
 fn main() {
 
     let (x_max, y_max) = (2usize, 50usize);
     let (x_maxi32, y_maxi32) = (x_max as i32, y_max as i32);
-    let (dx, dy) = (0.1, 0.1);
 
     let mut GD_grad_rho = VectorField2D::new(x_max, y_max);
     let mut GD_rho = ScalarField2D::new(x_max, y_max);    
@@ -124,8 +132,7 @@ fn main() {
                 .set_pos(xi, yi,
                          &grad_scalar(&GD_rho,
                                       xi as i32, yi as i32,
-                                      x_maxi32, y_maxi32,
-                                      dx, dy));
+                                      x_maxi32, y_maxi32));
             
         });});
 }
